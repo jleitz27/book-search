bd98946111c413943acb85afe0c84718233ddc6c
Jason L <jkleitz27@gmail.com>
2022-08-27 19:02:27 -0400

    paths should be working now

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
-
+const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
 const {authMiddleware} = require('./utils/auth');
@@ -11,7 +11,7 @@ const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware,
+  
 });
 
 const startApolloServer = async (typeDefs, resolvers) => {
