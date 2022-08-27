9617bbd6ce0f644f9852f465c007b6d60f8cd28c
Jason L <jkleitz27@gmail.com>
2022-08-27 19:09:40 -0400

    server.js updated

--- server/server.js
+++ server/server.js
@@ -13,13 +13,23 @@ const server = new ApolloServer({
   resolvers,
   
 });
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
 
-  app.use(express.urlencoded({ extended: false }));
-  app.use(express.json());
+  
 
   db.once('open', () => {
     app.listen(PORT, () => {
